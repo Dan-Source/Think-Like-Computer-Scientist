a2ae6e6102cd523d266057092e844a11d0a0ae9a
Daniel Luis <52837045+Dan-Source@users.noreply.github.com>
2020-02-11 20:23:14 -0300

    Meses do Ano

---
+++ meses_do_ano.py
@@ -0,0 +1,4 @@
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro','Outubro', 'Novembro', 'Dezembro']
+
+for i in meses:
+    print(" Um dos meses do ano é " + i + ".")
